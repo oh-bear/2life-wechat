08986ee41076ee4e80de625ecefb687582e83e7d
zyktrcn <594823346@qq.com>
2018-04-27 08:56:30 +0800

    fix detail

--- Pages/Home/Detail/Detail.js
+++ Pages/Home/Detail/Detail.js
@@ -18,12 +18,19 @@ Page({
     content: '真是幸福的一天啊。我一直酣睡到第二天的 10 点才从床上爬起来，吃了一点儿牛奶和燕麦，感觉身体充满了活力。',
     location: '燕塘站, 广东省, 中国',
     mood: '88',
-    like: false
+    like: false,
+    current: 0
   },
 
   /**
    * function
    */
+  swiperChange (event) {
+    var current = event.detail.current
+    this.setData({
+      current: current
+    })
+  },
   likeChange () {
     var current = this.data.like
     this.setData({
